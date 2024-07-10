9f0debdf8a337139ce5d681f6f20b1edf4d00cbf
byteflys <168255750+byteflys@users.noreply.github.com>
2024-07-10 21:22:54 +0800

    Delete app/build directory

--- app/build/generated/ap_generated_sources/debug/out/com/android/app/BR.java
+++
@@ -1,5 +0,0 @@
-package com.android.app;
-
-public class BR {
-  public static final int _all = 0;
-}
--- app/build/generated/ap_generated_sources/debug/out/com/android/library/skinner/BR.java
+++
@@ -1,5 +0,0 @@
-package com.android.library.skinner;
-
-public class BR {
-  public static final int _all = 0;
-}
